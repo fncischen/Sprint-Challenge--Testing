0d3f59ea0e5f23a40da9ed3e44c31945ba6758b9
Francis Chen <fncis.chen@gmail.com>
2019-04-12 09:40:59 -0700

    did tons of refactoring to ensure data would export

--- data/data.js
+++ data/data.js
@@ -1,4 +1,4 @@
-export default const data = [
+let data = [
     {
     title: 'Pacman', // required
     genre: 'Arcade', // required
@@ -23,3 +23,4 @@ export default const data = [
         releaseYear: 1995
     }
 ]
+
--- index.js
+++ index.js
@@ -2,12 +2,38 @@ const express = require("express");
 const server = express();
 const axios = require("axios");
 
-const db = require("./data/data.js");
+// const db = require("./data/data.js");
+
+let db = [
+    {
+    title: 'Pacman', // required
+    genre: 'Arcade', // required
+    releaseYear: 1980 // not required
+    },
+
+    {
+        title: 'Zelda', 
+        genre: 'Console', 
+        releaseYear: 1992 
+    },
+
+    {
+        title: 'Pokemon', 
+        genre: 'Console', 
+        releaseYear: 1985 
+    },
+
+    {
+        title: 'Doom', 
+        genre: 'PC', 
+        releaseYear: 1995
+    }
+]
 
 server.get("/games", (req,res) => {
     axios.get(db)
-        .then()
-        .catch();
+        .then(result => res.status(201).json(result))
+        .catch(err => res.status(500).json({errorMessage: "There was an error with posting data from the server"}));
 })
 
 server.post("/games", (req,res) => {
@@ -20,9 +46,9 @@ server.post("/games", (req,res) => {
 
         axios.post(db, req.body)
             .then(result => res.status(201).json(result))
-            .catch(err => res.status(500).json({errorMessage: "There was an error with retrieving data from the server"}));
+            .catch(err => res.status(500).json({errorMessage: "There was an error with posting data from the server"}));
     
         }
 })
 
-export default server;
+module.exports = server;
