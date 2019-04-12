ee7a16e47d574e24fed3a279cb598dcdabe62d84
Francis Chen <fncis.chen@gmail.com>
2019-04-12 08:39:38 -0700

    finished base server endpoints

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require("express");
 const server = express();
+const axios = require("axios");
 
 const db = require("./data/data.js");
 
-server.get("/games", () => {
-    
+server.get("/games", (req,res) => {
+    axios.get(db)
+        .then()
+        .catch();
 })
 
-server.post("/games", () => {
+server.post("/games", (req,res) => {
+    if(!req.body.title || !req.body.genre) {
 
+        res.status(422).json({errorMessage: "You are missing the title or genre of the game"})
+
+    }
+    else{
+
+        axios.post(db, req.body)
+            .then(result => res.status(201).json(result))
+            .catch(err => res.status(500).json({errorMessage: "There was an error with retrieving data from the server"}));
+    
+        }
 })
 
