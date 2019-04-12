c27bf1c63810c182f8d42b4bfb74b3af9eb8f426
Francis Chen <fncis.chen@gmail.com>
2019-04-12 08:31:44 -0700

    added server endpoints

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const server = express();
+
+const db = require("./data/data.js");
+
+server.get("/games", () => {
+    
+})
+
+server.post("/games", () => {
+
+})
+
